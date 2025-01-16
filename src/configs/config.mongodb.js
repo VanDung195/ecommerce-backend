a43bb7e986e8c6f9eae79cddcd4bab925f204607
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-16 12:36:13 +0700

    config(mongodb): add config

---
+++ src/configs/config.mongodb.js
@@ -0,0 +1,45 @@
+'use strict'
+require('dotenv').config()
+//lv0
+
+const config = {
+    app: {
+        port: 3000
+    },
+    db: {
+        host: 'localhost',
+        port: 27017,
+        name: 'ecommerce'
+    }
+};
+
+
+module.exports = config
+
+//lv01
+
+// const dev = {
+//     app: {
+//         port: process.env.DEV_APP_PORT || 3000
+//     },
+//     db: {
+//         host: process.env.DEV_DB_HOST || 'localhost',
+//         port: process.env.DEV_DB_PORT || 27017,
+//         name: process.env.DEV_DB_NAME || 'dbDev'
+//     }
+// };
+// const pro = {
+//     app: {
+//         port: process.env.PRO_APP_PORT || 3000
+//     },
+//     db: {
+//         host: process.env.PRO_DB_HOST || 'name',
+//         port: process.env.PRO_DB_PORT || 27017,
+//         name: process.env.PRO_DB_NAME || 'dbPro'
+//     }
+// };
+
+
+// const config = {dev, pro}
+// const env =  process.env.NODE_ENV || 'dev'
+// module.exports = config[env]
