487f02a9f2cf97c1fba0fca5ddfec8342f771bd9
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-16 19:22:43 +0700

    add

--- src/app.js
+++ src/app.js
@@ -12,4 +12,29 @@ app.use(express.urlencoded({
     extended: true, 
 }))
 
+//init db
+require('./dbs/init.mongodb')
+
+app.use('/', require('./routes'))
+
+
+//error handler
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    const statusCode = error.status || 500
+    return res.status(statusCode).json({
+        status: 'error',
+        code: statusCode,
+        stack: error.stack,
+        message: error.message || 'Internal Server Error'
+    })
+})
+
+
+
 module.exports = app
