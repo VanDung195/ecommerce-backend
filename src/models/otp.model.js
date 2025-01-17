5fc1a3c0b364d0a41d99a814076b747fda56eae4
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-17 14:40:19 +0700

    perf(db): improve usability by extending expireAt duration

--- src/models/otp.model.js
+++ src/models/otp.model.js
@@ -1,20 +1,45 @@
+// 'use strict'
+
+// const { Schema, model} = require('mongoose'); 
+
+// const DOCUMENT_NAME = 'otp_log'
+// const COLLECTION_NAME = 'otp_logs'
+
+// var otpSchema = new Schema({
+//     otp_token: { type: String, required: true},
+//     otp_email: { type: String, required: true},
+//     otp_status: { type: String, default: 'pending', enum: ['pending', 'active', 'block']},
+//     expireAt: {
+//         type: Date, default: Date.now(), expires: 3600
+//     }
+// }, {
+//    timestamps: true,
+//    collection: COLLECTION_NAME 
+// });
+
+// module.exports = model(DOCUMENT_NAME, otpSchema);
+
 'use strict'
 
-const { Schema, model} = require('mongoose'); // Erase if already required
+const { Schema, model } = require('mongoose'); // Erase if already required
 
 const DOCUMENT_NAME = 'otp_log'
 const COLLECTION_NAME = 'otp_logs'
 
 var otpSchema = new Schema({
-    otp_token: { type: String, required: true},
-    otp_email: { type: String, required: true},
-    otp_status: { type: String, default: 'pending', enum: ['pending', 'active', 'block']},
+    otp_token: { type: String, required: true },
+    otp_email: { type: String, required: true },
+    otp_status: { type: String, default: 'pending', enum: ['pending', 'active', 'block'] },
     expireAt: {
-        type: Date, default: Date.now(), expires: 180
+        type: Date,
+        default: function () {
+            return Date.now() + 3600000; // 3600000ms = 1 hour
+        },
+        expires: 3600 
     }
 }, {
-   timestamps: true,
-   collection: COLLECTION_NAME 
+    timestamps: true,
+    collection: COLLECTION_NAME
 });
 
 module.exports = model(DOCUMENT_NAME, otpSchema);
