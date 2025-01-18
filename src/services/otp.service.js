ff250caa43579646a14ad39e84ce8f4a47fd1f81
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-18 20:59:17 +0700

    update multiple functions

--- src/services/otp.service.js
+++ src/services/otp.service.js
@@ -1,14 +1,27 @@
 'use strict'
-
-const crypto = require('crypto')
-const { insertOtpLog } = require('../models/repositories/otp.repo')
+const { insertOtp, findOtpByToken, deleteToken, findOtpByEmail } = require('../models/repositories/otp.repo')
+const { BadRequestError } = require('../core/error.response')
 
 
 const newOtp = async (email) => {
-    const token = await insertOtpLog(email)
+    const token = await insertOtp(email)
     return token
+}
+const findOtpByTokenService = async ({ token}) => {
+    const otp = await findOtpByToken({ token })
+    return otp
+}
+const deleteTokenService = async({token}) => {
+    deleteToken({token})
+}
+const findOtpByEmailService = async ({ email}) => {
+    const otp = await findOtpByEmail({ email })
+    return otp
 }
 
 module.exports = {
-    newOtp
+    newOtp,
+    findOtpByTokenService,
+    findOtpByEmailService,
+    deleteTokenService
 }
