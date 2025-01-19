d955d146593745eec82c6c2492c8d90151864bb1
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-19 21:25:23 +0700

    feat: add keytoken service

---
+++ src/services/keytoken.service.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const { Types } = require('mongoose')
+const { findKeyTokenByUserId, createKeyToken } = require('../models/repositories/keytoken.repo')
+const { NotFoundError, BadRequestError } = require('../core/error.response')
+
+class KeyTokenService{
+    createKeyToken = async ({ userId, privateKey, publicKey, refreshToken }) => {
+        const keys = createKeyToken({
+            userId,
+            privateKey,
+            publicKey,
+            refreshToken
+        })
+        if(!keys) throw new BadRequestError('Create key failure')
+
+        return keys
+    }
+
+    findKeyTokenByUserId = async ({
+        userId
+    }) => {
+        const keyStore = await findKeyTokenByUserId({
+            userId
+        })
+        if(!keyStore) throw new NotFoundError('Key token not found');
+
+        return keyStore
+    }
+}
+
+module.exports = new KeyTokenService()
