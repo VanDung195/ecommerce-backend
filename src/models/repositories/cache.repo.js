dbacaac7b075fa639e6e522cd2ef5fe94ca584b5
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-19 21:23:32 +0700

    feat: add cache repo

---
+++ src/models/repositories/cache.repo.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const { getIORedis } = require('../../dbs/init.ioredis')
+const redisCache = getIORedis().instanceConnect
+
+const setCache = async ({
+    key,
+    value
+}) => {
+    if(!redisCache) throw new Error('Redis client not initialize')
+    try {
+        return await redisCache.set(key, value)
+    } catch (error) {
+        throw new Error(`${error.messgae}`)
+    }
+}
+
+const getCache = async ({
+    key
+}) => {
+    if(!redisCache) throw new Error('Redis client not initialize')
+    try {
+        return await redisCache.get(key)
+    } catch (error) {
+        throw new Error(`${error.messgae}`)
+    }
+}
+
+const setCacheExpiration = async ({
+    key,
+    value,
+    expiration
+}) => {
+    if(!redisCache) throw new Error('Redis client not initialize')
+    try {
+        return await redisCache.set(key, value, 'EX', expiration)
+    } catch (error) {
+        throw new Error(`${error.messgae}`)
+    }
+}
+
+module.exports = {
+    setCache,
+    getCache,
+    setCacheExpiration
+}
