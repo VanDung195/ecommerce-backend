b70ef0d6d6f765e9ded25db9e3ce44efa0b858d4
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-19 21:25:07 +0700

    feat: add new function

--- src/services/template.service.js
+++ src/services/template.service.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { htmlConfirmTokenEmail } = require('../utils/otp_email.html')
 const { findTemplate, insertTemplate} = require('../models/repositories/template.repo')
 const { BadRequestError} = require('../core/error.response')
 
@@ -17,15 +18,24 @@ const newTemplate = async ({
         //2. insert template into db
         const newTem = await insertTemplate({
             tem_name,
-            tem_html
+            tem_html: htmlConfirmTokenEmail
         })
         return newTem
     } catch (error) {
         return error
     }
 }
+const findTemplateService = async ({ 
+    tem_name,
+}) => {
+    const template = await findTemplate({
+        tem_name
+    })
+    if(!template) throw new BadRequestError('Template not found')
 
-
+    return template
+}
 module.exports = {
-    newTemplate
+    newTemplate,
+    findTemplateService
 }
