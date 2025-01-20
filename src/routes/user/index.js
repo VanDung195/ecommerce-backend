33a8c64c78c550e71401087a997319d9e1fe257d
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-20 23:13:57 +0700

    feat: add login router

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -8,6 +8,7 @@ const router = express.Router()
 router.post('/new_user', asyncHandler(UserController.newUser))
 // router.get('/welcome', asyncHandler(UserController.checkUserToken))
 router.post('/signup', asyncHandler(UserController.signUp))
+router.post('/login', asyncHandler(UserController.login))
 
 
 module.exports = router
