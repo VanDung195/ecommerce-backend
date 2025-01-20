d52e6cfab36fc30833f1b73aac06f6ad824b0f0b
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-20 23:13:38 +0700

    feat: add authen middleware

--- src/routes/template/index.js
+++ src/routes/template/index.js
@@ -3,8 +3,10 @@
 const express = require('express')
 const { asyncHandler} = require('../../helpers/asyncHandler')
 const templateController = require('../../controllers/template.controller')
+const { authentication } = require('../../auth/authUtils')
 const router = express.Router()
 
+router.use(authentication)
 
 router.post('/new_template', asyncHandler(templateController.newTem))
 
