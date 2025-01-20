82f7586d6746f46656d91e434e29b078dcbcaae1
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-20 23:12:17 +0700

    feat: add more token

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,7 @@ const router = express.Router()
 
 router.use('/api/template', require('./template'))
 router.use('/api/email', require('./email'))
+router.use('/api/user', require('./user'))
+router.use('/api', require('./access'))
 
 module.exports = router
