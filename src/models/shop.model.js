7e1c312268002a7c7e31160e61cf59742ec5efb2
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-22 20:57:55 +0700

    feat: add shop model

---
+++ src/models/shop.model.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const { Schema, model} = require('mongoose'); 
+
+const DOCUMENT_NAME = 'Shop'
+const COLLECTION_NAME = 'Shops'
+
+var shopSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true},
+    shop_name: { type: String, required: true},
+    shop_email: { type: String, required: true},
+    shop_address: { type: String, required: true},
+    shop_phone: { type: String, required: true},
+    shop_logo: { type: String, default: ''},
+    shop_status: { type: String, default: 'active', enum: ['active', 'inactive']},
+    shop_verify: { type: Boolean, default: false},
+    shop_created_at: { type: Date, default: Date.now},
+    shop_description: { type: String, default: ''},
+    shop_type: { type: String, default: 'individual', enum: ['individual', 'corporate']},
+    shop_ratings: { type: Number, default: 0},
+    followers: { type: Number, default: 0}
+}, { 
+   timestamps: true,
+   collection: COLLECTION_NAME 
+});
+
+module.exports = model(DOCUMENT_NAME, shopSchema);
