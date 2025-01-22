f1141c777d1a3030e4fa4663dc957698a36206eb
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-22 20:57:39 +0700

    feat: add login and logout controller

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,19 +1,35 @@
 'use strict'
 
 const { SuccessResponse } = require("../core/success.response")
-const {handlerRefreshToken} = require('../services/access.service')
+const { handlerRefreshTokenV2, logout, login } = require('../services/access.service')
 const HEADER = {
     CLIENT_ID: 'x-client-id',
     AUTHORIZATION: 'authorization'
 }
 
 class AccessController{
+    login = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'Login successfuly',
+            metadata: await login(req.body)
+        }).send(res)
+    }
+
     handlerRefreshToken = async(req, res, next) => {
         new SuccessResponse({
             message: 'Refresh token success',
-            metadata: await handlerRefreshToken({
+            metadata: await handlerRefreshTokenV2({
                 userIdHeader: req.headers[HEADER.CLIENT_ID]
             }) 
+        }).send(res)
+    }
+
+    logout = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Logout',
+            metadata: await logout({
+                keyStore: req.keyStore
+            })
         }).send(res)
     }
 }
