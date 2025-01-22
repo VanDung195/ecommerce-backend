c331502ec1a3fbbb4b1cfc6b97bc994214bc1053
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-22 20:59:33 +0700

    feat: add login router and middleware

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -3,7 +3,12 @@
 const express = require('express')
 const {asyncHandler} = require('../../helpers/asyncHandler')
 const AccessController = require('../../controllers/access.controller')
+const {authentication} = require('../../auth/authUtils')
 const router = express.Router()
+
+router.post('/user/login', asyncHandler(AccessController.login))
+
+router.use(authentication)
 
 router.post('/handler_refresh_token', asyncHandler(AccessController.handlerRefreshToken))
 
