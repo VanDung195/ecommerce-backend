63ca6faef9bd9aa5dcd65ae4ad368c83323747a4
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-23 21:09:40 +0700

    feat: add more router

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -3,12 +3,17 @@
 const {asyncHandler} = require('../../helpers/asyncHandler')
 const UserController = require('../../controllers/user.controller')
 const express = require('express')
+const { authentication } = require('../../auth/authUtils')
 const router = express.Router()
 
-router.post('/new_user', asyncHandler(UserController.newUser))
 // router.get('/welcome', asyncHandler(UserController.checkUserToken))
 router.post('/signup', asyncHandler(UserController.signUp))
-router.post('/login', asyncHandler(UserController.login))
+// router.post('/login', asyncHandler(UserController.login))
 
+router.use(authentication)
+
+router.post('/new_user', asyncHandler(UserController.newUser))
+router.post('/block_user', asyncHandler(UserController.blockUser))
+router.post('/unblock_user', asyncHandler(UserController.unBlockUser))
 
 module.exports = router
