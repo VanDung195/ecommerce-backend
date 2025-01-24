8904940c3fbfbfd760e3d9134aaf9bf7f19ae3f7
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-24 20:26:57 +0700

    feat: add shop and rbac router

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ const router = express.Router()
 router.use('/api/template', require('./template'))
 router.use('/api/email', require('./email'))
 router.use('/api/user', require('./user'))
+router.use('/api/shop', require('./shop'))
+router.use('/api/rbac', require('./rbac'))
 router.use('/api', require('./access'))
 
 module.exports = router
