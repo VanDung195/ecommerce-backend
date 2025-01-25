ecba7875d8fc1f6d9b613203cf6694390ec3fe58
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-25 23:08:06 +0700

    feat: add more routers

--- src/routes/rbac/index.js
+++ src/routes/rbac/index.js
@@ -5,12 +5,17 @@ const router = express.Router()
 const { asyncHandler } = require('../../helpers/asyncHandler')
 const {authentication} = require('../../auth/authUtils')
 const rbacController = require('../../controllers/rbac.controller')
+const { grantAccess } = require('../../middleware/rbac')
 
 router.use(authentication)
-router.post('/create_resource', asyncHandler(rbacController.newResource))
-router.get('/resources', asyncHandler(rbacController.listResources))
+router.post('/create_resource', grantAccess('createAny', 'resource'), asyncHandler(rbacController.newResource))
+router.get('/resources', grantAccess('readAny', 'resource'), asyncHandler(rbacController.listResources))
 
+router.get('/grants', asyncHandler(rbacController.getGrantsDetail))
 router.post('/create_role', asyncHandler(rbacController.newRole))
+router.get('/get_role_rbac_test', asyncHandler(rbacController.getRoleForRbac))
+router.get('/get_role_rbac_test_admin', grantAccess('readOwn', 'resource'), asyncHandler(rbacController.getRoleForRbac))
+router.post('/add_role_grant', asyncHandler(rbacController.addRoleGrant))
 
 
 module.exports = router
