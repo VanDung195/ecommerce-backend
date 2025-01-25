868a23c44029ca1b272988d1a532bbc98e24974c
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-25 23:05:46 +0700

    feat: add getRoleForRbac, getGrantsDetail, and addRoleGrant controllers

--- src/controllers/rbac.controller.js
+++ src/controllers/rbac.controller.js
@@ -1,6 +1,6 @@
 'use strict'
 const {SuccessResponse} = require('../core/success.response')
-const { createResourceService, listResroucesService, createRoleService } = require('../services/rbac.service')
+const { createResourceService, listResroucesService, createRoleService, getRoleForRbac, getGrantsDetailBySlugService, addRoleGrantService } = require('../services/rbac.service')
 
 class RBACController{
     newResource = async(req, res, next) => {
@@ -21,6 +21,27 @@ class RBACController{
         new SuccessResponse({
             message: 'Create new role successfuly',
             metadata: await createRoleService(req.body)
+        }).send(res)
+    }
+
+    getRoleForRbac = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Get role for rbac success',
+            metadata: await getRoleForRbac()
+        }).send(res)
+    }
+
+    getGrantsDetail = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Get grant success',
+            metadata: await getGrantsDetailBySlugService(req.query)
+        }).send(res)
+    }
+
+    addRoleGrant = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Add role grant success',
+            metadata: await addRoleGrantService(req.body)
         }).send(res)
     }
 }
