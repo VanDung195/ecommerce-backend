2ef95ae09a55769cb622d8b03b1e8ca48d383535
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-30 22:12:14 +0700

    feat: add getOneSpu controller

--- src/controllers/spu.controller.js
+++ src/controllers/spu.controller.js
@@ -1,14 +1,26 @@
 'use strict'
 
 const { SuccessResponse } = require('../core/success.response')
-const { createSpuService } = require('../services/spu.service')
+const { createSpuService, getOneSpuService } = require('../services/spu.service')
 
 class SpuController {
     newSpu = async(req, res, next) => {
-        const user = req.user
         new SuccessResponse({
             message: 'Create spu success',
-            metadata: await createSpuService(req.body)
+            metadata: await createSpuService({
+                userId: req.user.userId,
+                ...req.body
+            })
+        }).send(res)
+    }
+
+    oneSpu = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Get one spu success',
+            metadata: await getOneSpuService({
+                userId: req.user.userId,
+                ...req.params
+            })
         }).send(res)
     }
 }
