2e23dd67806ef1848c6d3cbec75e4d017cd604ea
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-30 22:29:17 +0700

    feat: add router

--- src/routes/spu/index.js
+++ src/routes/spu/index.js
@@ -4,7 +4,10 @@ const express = require('express')
 const router = express.Router()
 const { asyncHandler } = require('../../helpers/asyncHandler')
 const spuController = require('../../controllers/spu.controller')
+const { authentication } = require('../../auth/authUtils')
 
+router.use(authentication)
 router.post('/new_spu', asyncHandler(spuController.newSpu))
+router.get('/:slug', asyncHandler(spuController.oneSpu))
 
 module.exports = router
