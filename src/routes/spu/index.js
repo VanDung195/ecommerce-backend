e8afa76925696f94f771bc7c035d531d960ad697
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-01-31 20:09:38 +0700

    feat: update router

--- src/routes/spu/index.js
+++ src/routes/spu/index.js
@@ -5,9 +5,12 @@ const router = express.Router()
 const { asyncHandler } = require('../../helpers/asyncHandler')
 const spuController = require('../../controllers/spu.controller')
 const { authentication } = require('../../auth/authUtils')
+const { grantAccess } = require('../../middleware/rbac')
+
+router.get('/:slug', asyncHandler(spuController.oneSpu))
 
 router.use(authentication)
-router.post('/new_spu', asyncHandler(spuController.newSpu))
-router.get('/:slug', asyncHandler(spuController.oneSpu))
+router.get('/sku/detail', grantAccess('readOwn', 'product'), asyncHandler(spuController.listSkuBySpu));
+router.post('/spu/new_spu', grantAccess('createOwn', 'product'), asyncHandler(spuController.newSpu));
 
 module.exports = router
