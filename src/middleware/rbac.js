c1cd38c0499d7a9e1f72975c9c3d7162d8a67ca5
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-01 23:12:27 +0700

    feat: update get role name from DB

--- src/middleware/rbac.js
+++ src/middleware/rbac.js
@@ -2,14 +2,19 @@
 
 const AccessControl = require('accesscontrol');
 const { listRoleForRBAC } = require('../models/repositories/role.repo');
-const { AuthFailureError } = require('../core/error.response');
+const { AuthFailureError, NotFoundError } = require('../core/error.response');
+const { findUserById } = require('../models/repositories/user.repo');
 const rbac = new AccessControl()
 
 const grantAccess = (action, resource) => {
     return async(req, res, next) => {
         try {
             rbac.setGrants(await listRoleForRBAC())
-            const role_name = req.user.role
+            const userId = req.user.userId
+            const foundUser = await findUserById(userId) 
+            if(!foundUser) throw new AuthFailureError('User not registed')
+            const role_name = foundUser.usr_role
+            
             const permission = rbac.can(role_name)[action](resource)
             if (!permission || !permission.granted) {
                 throw new AuthFailureError('Permission denied');
