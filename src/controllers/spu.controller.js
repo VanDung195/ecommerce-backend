a5e068a0fbb584b24dbfe143e90bc827e04ea580
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-01 23:12:00 +0700

    feat: add more functions

--- src/controllers/spu.controller.js
+++ src/controllers/spu.controller.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { SuccessResponse } = require('../core/success.response')
-const { createSpuService, getOneSpuService } = require('../services/spu.service')
+const { createSpuService, getOneSpuService, getListSkuBySpuIdService, getOneSkuService, getAllSpuService, publishProductByShopService, unPublishProductByShopService } = require('../services/spu.service')
 
 class SpuController {
     newSpu = async(req, res, next) => {
@@ -17,9 +17,48 @@ class SpuController {
     oneSpu = async(req, res, next) => {
         new SuccessResponse({
             message: 'Get one spu success',
-            metadata: await getOneSpuService({
+            metadata: await getOneSpuService(req.params)
+        }).send(res)
+    }
+
+    listSkuBySpu = async(req, res, next) => {
+        console.log(req.query);
+        new SuccessResponse({
+            message: 'Get list sku success',
+            metadata: await getListSkuBySpuIdService(req.query)
+        }).send(res)
+    }
+
+    oneSku = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Get one sku success',
+            metadata: await getOneSkuService(req.body)
+        }).send(res)
+    }
+
+    allSpu = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Get list all spu success',
+            metadata: await getAllSpuService(req.query)
+        }).send(res)
+    }
+
+    publishProductByShop = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Publish product success',
+            metadata: await publishProductByShopService({
                 userId: req.user.userId,
-                ...req.params
+                ...req.body
+            })
+        }).send(res)
+    }
+
+    unPublishProductByShop = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Un publish product success',
+            metadata: await unPublishProductByShopService({
+                userId: req.user.userId,
+                ...req.body
             })
         }).send(res)
     }
