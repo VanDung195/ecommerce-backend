334b3c46fb6db40a712a74e22061edc0d0e20437
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-01 23:13:54 +0700

    feat: add more functions for product

--- src/services/spu.service.js
+++ src/services/spu.service.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const { NotFoundError, BadRequestError } = require('../core/error.response')
-const { findShopById } = require("../models/repositories/shop.repo")
-const { createSku } = require('../models/repositories/sku.repo')
-const { createSpu } = require('../models/repositories/spu.repo')
+const { findShopById, findShopByUserId } = require("../models/repositories/shop.repo")
+const { createSku, getAllSkuBySpuId, getOneSku } = require('../models/repositories/sku.repo')
+const { createSpu, getOneSpuBySlug, getAllSpu, getOneSpuById, publishProductByShop, unPublishProductByShop } = require('../models/repositories/spu.repo')
 const { findUserById } = require('../models/repositories/user.repo')
 const { convertToObjectIdMongodb } = require("../utils")
 
@@ -22,11 +22,11 @@ const createSpuService = async({
     if(!foundUser) throw new NotFoundError('User not found')
 
     //check shop role (vi du viet middleware sau)
-    if(foundShop.usr_role !== 'shop') throw new BadRequestError('You dont have permisssion')
 
-    const foundShop = await findShopById({ shopId: convertToObjectIdMongodb(foundUser._id)})
+    const foundShop = await findShopById({ userId: convertToObjectIdMongodb(foundUser._id)})
     if(!foundShop) throw new NotFoundError('Shop not found')
-        
+    if(foundUser.usr_role !== 'shop') throw new BadRequestError('You dont have permisssion')
+    const shop = foundShop._id
     const spu = await createSpu({
         shop, 
         name, 
@@ -39,18 +39,174 @@ const createSpuService = async({
     })
     if(!spu) throw new BadRequestError('Create spu failed')
     if(spu && sku_list.length > 0) {
-        //create sku
-        const sku = await createSku({
-            spuId: spu._id,
-            sku_list,
-            shopId: shop
-        })
-
-        if(!sku) throw new BadRequestError('Create sku failed')
+        try {
+            //create sku
+            const sku = await createSku({
+                spuId: spu._id,
+                sku_list,
+                shopId: shop
+            })
+        } catch (error) {
+            if(!sku) throw new BadRequestError('Create sku failed')
+        }
     }
     return spu
 }
 
+const getOneSpuService = async({
+    slug
+}) => {
+    const foundSpu = await getOneSpuBySlug(slug)
+    if(!foundSpu) throw new NotFoundError('Spu not found')
+    if(foundSpu.isDeleted) throw new BadRequestError('This product has been deleted') 
+    const productId = foundSpu._id
+    const listSku = await getAllSkuBySpuId({ spuId: productId})
+    
+    if(!listSku) throw new NotFoundError('Sku not found')
+
+    return {
+        spu: foundSpu,
+        sku: listSku
+    }
+}
+
+//this service for shop
+const getListSkuBySpuIdService = async({
+    spuId
+}) => {
+    const sku = await getAllSkuBySpuId({
+        spuId: convertToObjectIdMongodb(spuId),
+        unSelect: ['isDeleted', 'createdAt', 'updatedAt', '__v']
+    })
+    if(!sku) throw new NotFoundError('Sku not found')
+    return sku
+}
+
+const getOneSkuService = async({
+    spuId,
+    skuId
+}) => {
+    const sku = await getOneSku({
+        spuId,
+        skuId,
+        unSelect: ['isDeleted', 'createdAt', 'updatedAt', '__v']
+    })
+    console.log(sku);
+    
+    if(!sku) throw new NotFoundError('Sku not found')
+    return sku
+}
+
+const deleteSpuService = async() => {
+
+}
+
+const getAllSpuService = async({
+    limit = 30,
+    page = 1,
+    search = ''
+}) => {
+    const spus = await getAllSpu({
+        limit,
+        page,
+        search,
+        unSelect: ['__v', 'isDeleted', 'createdAt', 'updatedAt']
+    })
+    if(!spus) throw new NotFoundError('Spus not found')
+    return spus
+}
+
+const updateInventoryStockSpuService = async() => {
+
+}
+
+const updateInventoryStockSkuService = async() => {
+
+}
+
+const publishProductByShopService = async({
+    userId,
+    spuId
+}) => {
+    const foundShop = await findShopByUserId({ userId })
+    if(!foundShop) throw new NotFoundError('Shop not found')
+    
+    const shopId = foundShop._id
+    const foundProduct = await getOneSpuById({
+        shop: shopId,
+        spuId
+    })
+    if(!foundProduct) throw new NotFoundError('Product not found')
+    if(foundProduct.isPublished) throw new BadRequestError('Product has been publish')
+    foundProduct.isDraft = false
+    foundProduct.isPublished = true
+
+    const modifiedCount = await publishProductByShop({ product: foundProduct })
+    if(modifiedCount <= 0) throw new BadRequestError('Publish product failure')
+
+    return modifiedCount
+}
+
+const unPublishProductByShopService = async({
+    userId,
+    spuId
+}) => {
+    const foundShop = await findShopByUserId({ userId })
+    if(!foundShop) throw new NotFoundError('Shop not found')
+    
+    const shopId = foundShop._id
+    const foundProduct = await getOneSpuById({
+        shop: shopId,
+        spuId
+    })
+    if(!foundProduct) throw new NotFoundError('Product not found')
+    if(foundProduct.isDraft) throw new BadRequestError('Product has been unpublish')
+    foundProduct.isDraft = true
+    foundProduct.isPublished = false
+
+    const modifiedCount = await unPublishProductByShop({ product: foundProduct })
+    if(modifiedCount <= 0) throw new BadRequestError('unPublish product failure')
+
+    return modifiedCount
+}
+
+const getAllDraftsForShopService = async({
+    
+}) => {
+
+}
+
+const getAllPublicForShopService = async({
+
+}) => {
+
+}
+
+const getAllProductForShopService = async({
+
+}) => {
+
+}
+
+const updateSpuService = async() => {
+
+}
+
+const updateSkuservice = async() => {
+
+}
+
+//Hiển thị cho khách hàng mua
+const getAllSpuForClient = async() => {
+
+}
+
 module.exports = {
-    createSpuService
+    createSpuService,
+    getOneSpuService,
+    getListSkuBySpuIdService,
+    getOneSkuService,
+    getAllSpuService,
+    publishProductByShopService,
+    unPublishProductByShopService
 }
