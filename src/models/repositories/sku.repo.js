4133f6214b3081aca29f9c7762c5e171a5827ba5
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-03 22:35:50 +0700

    feat: add function for spu and sku

--- src/models/repositories/sku.repo.js
+++ src/models/repositories/sku.repo.js
@@ -43,12 +43,228 @@ const getOneSku = async({
     const sku = await SKU.findOne({
         productId: convertToObjectIdMongodb(spuId),
         skuId
-    }).select(unSelectData(unSelect)).lean()
+    })
+    .populate('productId', 'product_shop -_id')
+    .select(unSelectData(unSelect))
+    .lean()
     return sku
 }
+
+const createOneSku = async({
+    shopId,
+    spuId,
+    skuId,
+    sku_tier_idx,
+    sku_price,
+    sku_stock
+}) => {
+
+}
+
+const deleteSku = async({
+    shopId,
+    spuId,
+    skuId
+}) => {
+
+}
+
+const deleteListSku = async({
+    shopId,
+    spuId
+}) => {
+    
+}
+
+const getTotalInvenStockAndPrice = async({
+    productId
+}) => {
+    const productObjectId = convertToObjectIdMongodb(productId)
+    const result = await SKU.aggregate([
+        {
+            $match: {
+                productId: productObjectId,
+                isDeleted: false
+            }
+        },
+        {
+            $group: {
+                _id: '$productId',
+                totalQuantity: {
+                    $sum: '$sku_stock'
+                },
+                minPrice: {
+                    $min: '$sku_price'
+                },
+                maxPrice: {
+                    $max: '$sku_price'
+                }
+            }
+        }
+    ])
+    return result[0]
+}
+
+const getTotalInvenStock = async({
+    productId
+}) => {
+    const productObjectId = convertToObjectIdMongodb(productId)
+    const result = await SKU.aggregate([
+        {
+            $match: {
+                productId: productObjectId,
+                isDeleted: false
+            }
+        },
+        {
+            $group: {
+                _id: '$productId',
+                totalQuantity: {
+                    $sum: '$sku_stock'
+                }
+            }
+        }
+    ])
+    return result[0]
+}
+
+const getMinAndMaxPrice = async({
+    productId
+}) => {
+    const productObjectId = convertToObjectIdMongodb(productId)
+    const result = await SKU.aggregate([
+        {
+            $match: {
+                productId: productObjectId,
+                isDeleted: false
+            }
+        },
+        {
+            $group: {
+                _id: "$productId",
+                minPrice: {
+                    $min: '$sku_price'
+                },
+                maxPrice: {
+                    $max: '$sku_price'
+                }
+            }
+        }
+    ])
+    return result[0]
+}
+
+const updateOneSku = async({
+    productId,
+    skuId,
+    sku_tier_idx,
+    sku_price,
+    sku_stock
+}) => {
+    const result = await SKU.findOneAndUpdate(
+        {
+            productId,
+            skuId
+        },
+        {
+            $set: {
+                sku_tier_idx,
+                sku_price,
+                sku_stock
+            }
+        },
+        {
+            new: true
+        }
+    )
+    return result
+}
+
+const checkSkuByServer = async({
+    productId,
+    listSku
+}) => {
+    const checkSpus = await Promise.all(
+        listSku.map( async sku => {
+            const foundSku = await getOneSku({
+                spuId: productId,
+                skuId: sku.skuId
+            })
+            if(foundSku){
+                return foundSku
+            }
+        })
+    )
+    return checkSpus
+}
+
+const updateListSku = async ({ 
+    productId, 
+    listSku 
+}) => {
+    const updatedSkus = await Promise.all(
+        listSku.map(async sku => {
+            return await SKU.findOneAndUpdate(
+                {
+                    productId: convertToObjectIdMongodb(productId),
+                    skuId: sku.skuId
+                },
+                {
+                    $set: {
+                        sku_tier_idx: sku.sku_tier_idx,
+                        sku_price: sku.sku_price,
+                        sku_stock: sku.sku_stock
+                    }
+                },
+                { new: true } 
+            )
+        })
+    )
+    return updatedSkus
+}
+
+const updateListSkuV2 = async({
+    productId,
+    listSku
+}) => {
+    const bulkOps = listSku.map( sku => ({
+        updateOne: {
+            filter: {
+                productId: convertToObjectIdMongodb(productId),
+                skuId: sku.skuId
+            },
+            update: {
+                $set: {
+                    sku_tier_idx: sku.sku_tier_idx,
+                    sku_price: sku.price,
+                    sku_stock: sku.stock
+                }
+            }
+        }
+    }))
+    const result = await SKU.bulkWrite(bulkOps)
+    return result
+}
+
+
+const publishSku = async() => {
+
+}
+
+const unPublishSku = async() => {
+
+}
+
 
 module.exports = {
     createSku,
     getAllSkuBySpuId,
-    getOneSku
+    getOneSku,
+    getTotalInvenStock,
+    getMinAndMaxPrice,
+    getTotalInvenStockAndPrice,
+    updateOneSku,
+    updateListSku,
+    updateListSkuV2,
+    checkSkuByServer
 }
