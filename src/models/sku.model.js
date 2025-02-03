b22b56ffd45878dc10150e2a313e8f9eb8c2a617
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-03 22:41:32 +0700

    feat: add validators

--- src/models/sku.model.js
+++ src/models/sku.model.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const { Schema, Types, model} = require('mongoose'); // Erase if already required
-const slugify = require('slugify')
+const slugify = require('slugify');
+// const { updateInvenStockSpuService } = require('../services/spu.service');
 
 const COLLECTION_NAME = 'Skus'
 const DOCUMENT_NAME = 'Sku'
@@ -11,8 +12,8 @@ var skuSchema = new Schema({
     sku_tier_idx: { type: Array, default: [0]},
     sku_default: { type: Boolean, default: false},
     sku_slug: { type: String, default: ''},
-    sku_price: { type: String, required: true},
-    sku_stock: { type: Number, required: true},
+    sku_price: { type: Number, required: true, min: 1000},
+    sku_stock: { type: Number, required: true, min: 1},
     productId: { type: Schema.Types.ObjectId, ref: 'Spu'},
     isDraft: { type: Boolean, default: true, index: true, select: false},
     isPublished: { type: Boolean, default: false, index: true, select: false},
@@ -21,5 +22,21 @@ var skuSchema = new Schema({
     timestamps: true,
     collection: COLLECTION_NAME
 });
+
+// skuSchema.post('save', async(next) => {
+//     if(!this.isModified('sku_price')){
+//         try {
+//             //update min price and max price value of spuSchema
+//             const result = await updateInvenStockSpuService(this.productId)
+//             return next()
+//         } catch (error) {
+//             return next(error)
+//         }
+//     }
+//     if(!this.isModified('sku_stock')){
+//         //update total stock value of spuSchema
+//         return next()
+//     }
+// })
 
 module.exports = model(DOCUMENT_NAME, skuSchema);
