c8cecdda8fa9ae53a69ffcf736b8476b2ec12423
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-07 22:52:14 +0700

    feat: add inventory schema

---
+++ src/models/inventory.model.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const { Schema, model} = require('mongoose'); 
+
+const DOCUMENT_NAME = 'Inventory'
+const COLLECTION_NAME = 'Inventories'
+
+var inventorySchema = new Schema({
+    inven_productId: { type: String, ref: 'Sku', required: true, unique: true},
+    inven_stock: { type: Number, required: true, min: 0},
+    inven_shopId: { type: Schema.Types.ObjectId, ref: 'Shop', required: true},
+    inven_sold: { type: Number, default: 0},
+    inven_status: { type: String, enum: ['in_stock', 'low_stock', 'out_of_stock'], default: 'instock'}
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+});
+
+module.exports = model(DOCUMENT_NAME, inventorySchema);
