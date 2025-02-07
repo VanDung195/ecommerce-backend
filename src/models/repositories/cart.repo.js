07bce4262478ced6a9d496ecefbe0ed82ddec174
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-07 22:53:30 +0700

    feat: update functions for handler cart

--- src/models/repositories/cart.repo.js
+++ src/models/repositories/cart.repo.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const { convertToObjectIdMongodb } = require('../../utils')
+const { convertToObjectIdMongodb, unSelectData } = require('../../utils')
 const CART = require('../cart.model')
 
-const createCart = async({
+const createCart = async ({
     userId,
     product
 }) => {
@@ -16,29 +16,69 @@ const createCart = async({
     return newCart
 }
 
-const addProductToCart = async({
-    cartId,
-    userId,
-    products
-}) => {
-
-}
-
-const updateProductQuantity = async({
-    cartId,
-    userId,
-    product
-}) => {
+const updateProductQuantity = async ({ cartId, userId, product }) => {
     const cart = await CART.findOneAndUpdate(
         {
             _id: convertToObjectIdMongodb(cartId),
             userId: convertToObjectIdMongodb(userId),
             cart_state: 'active',
             'cart_products.productId': product.productId
-        }, 
+        },
         {
             $inc: {
-                'cart_products.quantity': product.quantity
+                'cart_products.$[elem].quantity': product.quantity
+            }
+        },
+        {
+            new: true,
+            arrayFilters: [{ 'elem.productId': product.productId }]
+        }
+    )
+    return cart
+}
+
+const getOneCartByUserId = async ({
+    userId,
+}) => {
+    const cart = await CART.findOne({
+        userId: convertToObjectIdMongodb(userId)
+    })
+    return cart
+}
+
+const updateCartCount = async ({
+    userId,
+    quantity
+}) => {
+    return await CART.findOneAndUpdate(
+        {
+            userId
+        },
+        {
+            $inc: {
+                cart_count_product: quantity
+            }
+        },
+        {
+            new: true
+        }
+    )
+}
+
+const removeFromCart = async ({
+    userId,
+    productId
+}) => {
+    const cart = await CART.findOneAndUpdate(
+        {
+            userId,
+            'cart_products.productId': productId
+        },
+        {
+            $pull: {
+                'cart_products': {
+                    productId: productId
+                }
             }
         },
         {
@@ -48,19 +88,67 @@ const updateProductQuantity = async({
     return cart
 }
 
-const getOneCartByUserId = async({
+const clearCart = async ({
     userId
 }) => {
-    const cart = await CART.findOne(
+    const cart = await CART.findOneAndUpdate(
         {
-            cart_userId: convertToObjectIdMongodb(userId)
+            userId
+        },
+        {
+            $set: {
+                cart_products: []
+            }
+        },
+        {
+            new: true
         }
     )
     return cart
 }
 
+const getListProductFromCart = async ({
+    userId,
+    unSelect = []
+}) => {
+    const cart = await CART.aggregate([
+        {
+            $match: {
+                userId: convertToObjectIdMongodb(userId)
+            }
+        },
+        {
+            $unwind: '$cart_products'
+        },
+        {
+            $lookup: {
+                from: 'Skus',
+                localField: 'cart_products.productId',
+                foreignField: 'skuId',
+                as: 'variant'
+            }
+        },
+        {
+            $unwind: '$variant'
+        },
+        {
+            $project: {
+                _id: 0,
+                cart_state: 0,
+                __v: 0
+            }
+        }
+    ])
+    return cart
+}
+
+
 module.exports = {
     createCart,
     getOneCartByUserId,
-    updateProductQuantity
+    updateProductQuantity,
+    removeFromCart,
+    clearCart,
+    updateCartCount,
+    getListProductFromCart
 }
