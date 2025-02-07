391cfb8e9ca564e6e51c9e582c2ef904022e7c32
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-07 22:52:37 +0700

    feat: update controller for handler cart

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,16 +1,50 @@
 'use strict'
 
 const { SuccessResponse } = require("../core/success.response")
-const { addToCartService } = require("../services/cart.service")
+const { addToCartService, updateCartQuantityService, removeFromCartService, clearCartService, listToCartService } = require("../services/cart.service")
 
 class CartController{
+    listToCart = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Get list product successfully',
+            metadata: await listToCartService({ userId: req.user.userId })
+        }).send(res)
+    }
+
     addToCart = async(req, res, next) => {
         new SuccessResponse({
             message: 'Add to cart successfully',
             metadata: await addToCartService({
                 userId: req.user.userId,
+                product: req.body
+            })
+        }).send(res)
+    }
+
+    updateQuantity = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Update quantity successfully',
+            metadata: await updateCartQuantityService({
+                userId: req.user.userId,
                 ...req.body
             })
+        }).send(res)
+    }
+
+    removeFromCart = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Remove product from cart successfully',
+            metadata: await removeFromCartService({
+                userId: req.user.userId,
+                ...req.body
+            })
+        }).send(res)
+    }
+
+    clearCart = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Clear cart successfully',
+            metadata: await clearCartService({ userId: req.user.userId})
         }).send(res)
     }
 }
