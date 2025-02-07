cc63214b0ef8cd0fab1da0d7bf1a7ad32bdc6074
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-07 22:54:50 +0700

    feat: add routers for handler cart

--- src/routes/cart/index.js
+++ src/routes/cart/index.js
@@ -7,6 +7,10 @@ const { authentication } = require('../../auth/authUtils')
 const cartController = require('../../controllers/cart.controller')
 
 router.use(authentication)
+
 router.post('/add_to_cart', asyncHandler(cartController.addToCart))
+router.delete('/clear', asyncHandler(cartController.clearCart))
+router.post('/update', asyncHandler(cartController.updateQuantity))
+router.get('', asyncHandler(cartController.listToCart))
 
 module.exports = router
