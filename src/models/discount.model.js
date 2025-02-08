8b37854808433dfa2a01729dc7ca0381afd72a33
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-08 22:08:33 +0700

    feat: add discount model

---
+++ src/models/discount.model.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const { Schema, model} = require('mongoose'); // Erase if already required
+
+const DOCUMENT_NAME = 'Discount'
+const COLLECTION_NAME = 'Discounts'
+
+// Declare the Schema of the Mongo model
+var discountSchema = new Schema({
+    discount_name: { type: String, required: true},
+    discount_description: { type: String, required: true},
+    discount_type: { type: String, enum: ['fixed_amount', 'percentage']},
+    discount_max_amount: {
+        type: Number,
+        required: function(){
+            return this.discount_type === 'percentage'
+        }
+    },
+    discount_value: { type: Number, required: true},
+    discount_code: { type: String, required: true, index: true},
+    discount_start_date: { type: Date, required: true},
+    discount_end_date: { type: Date, required: true},
+    discount_max_uses: { type: Number, required: true},
+    discount_uses_count: { type: Number, default: 0},
+    discount_user_used: { type: Array, default: []},
+    discount_max_use_per_user: { type: Number, required: true},
+    discount_min_order_value: { type: Number, required: true},
+    discount_shopId: { type: Schema.Types.ObjectId, ref: 'Shop', required: true, index: true},
+    discount_is_active: { type: Boolean, default: true, index: true},
+    discount_applies_to: { type: String, enum: ['all', 'specific']},
+    discount_productIds: { type: [Schema.Types.ObjectId], ref: 'Sku', required: true}
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+});
+
+module.exports = model(DOCUMENT_NAME, discountSchema);
