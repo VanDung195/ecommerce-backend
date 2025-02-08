8717478e66742fe2a2fe84029b598a9018708c24
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-08 22:09:14 +0700

    feat: update getListFromCart and toggle selection functions

--- src/models/repositories/cart.repo.js
+++ src/models/repositories/cart.repo.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { convertToObjectIdMongodb, unSelectData } = require('../../utils')
+const { convertToObjectIdMongodb } = require('../../utils')
 const CART = require('../cart.model')
 
 const createCart = async ({
@@ -17,6 +17,8 @@ const createCart = async ({
 }
 
 const updateProductQuantity = async ({ cartId, userId, product }) => {
+    console.log(product);
+
     const cart = await CART.findOneAndUpdate(
         {
             _id: convertToObjectIdMongodb(cartId),
@@ -67,12 +69,14 @@ const updateCartCount = async ({
 
 const removeFromCart = async ({
     userId,
-    productId
+    productId,
+    shopId
 }) => {
     const cart = await CART.findOneAndUpdate(
         {
             userId,
-            'cart_products.productId': productId
+            'cart_products.productId': productId,
+            'cart_products.shopId': shopId
         },
         {
             $pull: {
@@ -109,7 +113,6 @@ const clearCart = async ({
 
 const getListProductFromCart = async ({
     userId,
-    unSelect = []
 }) => {
     const cart = await CART.aggregate([
         {
@@ -125,20 +128,126 @@ const getListProductFromCart = async ({
                 from: 'Skus',
                 localField: 'cart_products.productId',
                 foreignField: 'skuId',
-                as: 'variant'
+                as: 'variantInfo'
             }
         },
         {
-            $unwind: '$variant'
+            $unwind: '$variantInfo'
         },
         {
             $project: {
                 _id: 0,
                 cart_state: 0,
-                __v: 0
+                __v: 0,
+                userId: 0,
+                'variantInfo._id': 0,
+                'variantInfo.isDraft': 0,
+                'variantInfo.isPublished': 0,
+                'variantInfo.isDeleted': 0,
+                'variantInfo.createdAt': 0,
+                'variantInfo.updatedAt': 0,
+                'variantInfo.__v': 0,
+                'variantInfo.skuId': 0,
+            }
+        },
+        {
+            $lookup: {
+                from: 'Spus',
+                localField: 'variantInfo.productId',
+                foreignField: '_id',
+                as: 'productInfo'
+            }
+        },
+        {
+            $project: {
+                'productInfo._id': 0,
+                'productInfo.isDraft': 0,
+                'productInfo.isPublished': 0,
+                'productInfo.isDeleted': 0,
+                'productInfo.createdAt': 0,
+                'productInfo.updatedAt': 0,
+                'productInfo.__v': 0,
+            }
+        },
+        {
+            $unwind: '$productInfo'
+        },
+        {
+            $project: {
+                cart_count_product: 1,
+                product: {
+                    name: '$productInfo.product_name',
+                    price: '$productInfo.product_price',
+                    quantity: '$cart_products.quantity',
+                    shopId: '$productInfo.product_shop',
+                    productId: '$cart_products.productId',
+                    isSelected: '$cart_products.isSelected',
+                    productVariations: '$productInfo.product_variations',
+                    totalPrice: {
+                        $multiply: ['$productInfo.product_price', '$cart_products.quantity']
+                    },
+                    variant: '$variantInfo'
+                },
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                cart_count_product: {
+                    $first: '$cart_count_product',
+                },
+                products: {
+                    $push: '$product'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0
             }
         }
     ])
+    return cart
+}
+
+const selectProductFromCart = async ({ userId, shopId, productId }) => {
+    const cart = await CART.findOneAndUpdate(
+        {
+            userId,
+            'cart_products.productId': productId,
+            'cart_products.shopId': shopId
+        },
+        [
+            {
+                $set: {
+                    cart_products: {
+                        $map: {
+                            input: "$cart_products",
+                            as: "item",
+                            in: {
+                                $cond: {
+                                    if: {
+                                        $and: [
+                                            { $eq: ["$$item.productId", productId] },
+                                            { $eq: ["$$item.shopId", shopId] }
+                                        ]
+                                    },
+                                    then: {
+                                        $mergeObjects: [
+                                            "$$item",
+                                            { isSelected: { $not: ["$$item.isSelected"] } }
+                                        ]
+                                    },
+                                    else: "$$item"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        ],
+        { new: true }
+    )
     return cart
 }
 
@@ -150,5 +259,6 @@ module.exports = {
     removeFromCart,
     clearCart,
     updateCartCount,
-    getListProductFromCart
+    getListProductFromCart,
+    selectProductFromCart
 }
