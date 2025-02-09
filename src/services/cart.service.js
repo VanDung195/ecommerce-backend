d4241ab23f568904fcc8a223007e44160e15e282
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-09 21:44:23 +0700

    refactor: update functions for carts

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { BadRequestError, NotFoundError } = require('../core/error.response')
-const { getOneCartByUserId, createCart, updateProductQuantity, removeFromCart, clearCart, updateCartCount, getListProductFromCart, selectProductFromCart } = require("../models/repositories/cart.repo")
+const { getOneCartByUserId, createCart, updateProductQuantity, removeFromCart, clearCart, updateCartCount, getListProductFromCart, selectProductFromCart, updateProductQuantityV2, removeCartShop } = require("../models/repositories/cart.repo")
 const { findShopById, findShopByShopId } = require('../models/repositories/shop.repo')
 const { getOneSkuById } = require('../models/repositories/sku.repo')
 
@@ -27,31 +27,58 @@ const addToCartService = async({
     if(!userCart){
         return await createCart({
             userId,
-            product :{
+            product_shop: [
+                {
+                    shopId: product.shopId,              
+                    // shop_discount: null,
+                    product_shop: [                       
+                        {
+                            productId: product.productId,
+                            name: foundProduct.productId.product_name,
+                            price: foundProduct.sku_price,
+                            isSelected: false,
+                            quantity: product.quantity
+                        }
+                    ]
+                }
+            ]
+        })
+    }
+    const existsProductInCart = userCart.cart_products.some(shop => 
+        shop.product_shop.some(productItem => productItem.productId === product.productId)
+    )
+    if(!existsProductInCart){
+        userCart.cart_count_product = userCart.cart_count_product + 1
+        let shopInCart = userCart.cart_products.find(shop => shop.shopId.toString() === product.shopId)
+        if(shopInCart){
+            shopInCart.product_shop.push({
+                productId: product.productId,
                 name: foundProduct.productId.product_name,
                 price: foundProduct.sku_price,
                 isSelected: false,
-                ...product
-            }
-        })
+                quantity: product.quantity
+            })
+        } else {
+            userCart.cart_products.push({
+                shopId: product.shopId,
+                // shop_discount: {},
+                product_shop: [
+                    {
+                        productId: product.productId,
+                        name: foundProduct.productId.product_name,
+                        price: foundProduct.sku_price,
+                        isSelected: false,
+                        quantity: product.quantity
+                    }
+                ]
+            })
+        }
+        return await userCart.save()
     }
-    const existsProductInCart = userCart.cart_products.some(productInCart => productInCart.productId === product.productId);
-    if(!existsProductInCart){
-        userCart.cart_count_product = userCart.cart_count_product + 1
-        userCart.cart_products.push({
-            name: foundProduct.productId.product_name,
-            price: foundProduct.sku_price,
-            isSelected: false,
-            ...product
-        })
-        const cart = await userCart.save()
-        return cart
-    }
-
-    return await updateProductQuantity({
+    return await updateProductQuantityV2({
         cartId: userCart._id,
         userId,
-        product
+        product_shop: product
     })
 }
 
@@ -66,26 +93,37 @@ const addToCartService = async({
 */
 const updateCartQuantityService = async({
     userId,
-    shopProduct
+    product
 }) => {
     const foundCart = await getOneCartByUserId({ userId })
     if(!foundCart) 
         throw new NotFoundError('Cart not found')
-    const foundProduct = await getOneSkuById(shopProduct.productId)
-    if(foundProduct.productId.product_shop.toString() !== shopProduct.shopId)
+    const foundProduct = await getOneSkuById(product.productId)
+    const shopId = foundProduct.productId.product_shop
+    if(foundProduct.productId.product_shop.toString() !== product.shopId)
         throw new BadRequestError('Invalid shop')
     if(!foundProduct)
         throw new NotFoundError('Product not found')
-    if(shopProduct.quantity === 0)
-        return await removeFromCart({ userId, productId: shopProduct.productId })
-    shopProduct.quantity = shopProduct.quantity - shopProduct.old_quantity
-    const cart = await updateProductQuantity({
+    if(product.quantity === 0){
+        const delCart = await removeFromCart({ userId, productId: product.productId, shopId: shopId.toString() })
+        const shopIndex = delCart.cart_products.findIndex( shop => shop.shopId.toString() === shopId.toString())
+        if(shopIndex !== -1){
+            const shopProducts = delCart.cart_products[shopIndex].product_shop
+            if(shopProducts.length === 0){
+                await removeCartShop({ userId, shopId })
+            }
+        }
+
+        return delCart
+    }
+    product.quantity = product.quantity - product.old_quantity
+    const cart = await updateProductQuantityV2({
         cartId: foundCart._id,
         userId,
-        product: shopProduct
+        product_shop: product
     })
     if(cart.cart_count_product <= 0)
-        return await removeFromCart({ userId, productId: shopProduct.productId })
+        return await removeFromCart({ userId, productId: product.productId })
     return cart
 }
 
@@ -101,7 +139,16 @@ const removeFromCartService = async({
     if(!foundProduct)
         throw new NotFoundError('Product not found')
     await updateCartCount({ userId, quantity: -1})
-    return await removeFromCart({ userId, productId, shopId})
+    const delCart = await removeFromCart({ userId, productId, shopId})
+
+    const shopIndex = delCart.cart_products.findIndex( shop => shop.shopId.toString() === shopId)
+    if(shopId !== -1){
+        const shopProducts = delCart.cart_products[shopIndex].product_shop
+        if(shopProducts.length === 0){
+            await removeCartShop({ userId, shopId })
+        }
+    }
+    return delCart
 }
 
 const clearCartService = async({
