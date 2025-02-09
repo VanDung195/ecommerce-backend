227f9f749c68aa9a6dbb2e6be2756021082f78ab
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-09 21:39:37 +0700

    refactor: change schema structure

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -5,6 +5,12 @@ const { model, Schema} = require('mongoose')
 const DOCUMENT_NAME = 'Cart'
 const COLLECTION_NAME = 'Carts'
 
+const productCartSchema = new Schema({
+    shopId: { type: Schema.Types.ObjectId, ref: 'Shop', required: true},
+    shop_discount: { type: Object, default: null},
+    product_shop: { type: Array, default: []}
+}, { _id: 0 })
+
 const cartSchema = new Schema({
     cart_state: {
         type: String,
@@ -12,9 +18,30 @@ const cartSchema = new Schema({
         enum: ['active', 'complete', 'failed', 'pending'],
         default: 'active'
     },
-    cart_products: {type: Array, required: true, default: []},
-    cart_count_product: {type: Number, default: 0},
-    cart_userId: {type: Number, required: true}
+    cart_products: {type: [productCartSchema], required: true},
+    /*
+        [
+            {
+                shopId,
+                shop_discount: {
+                    shopId,
+                    discountId,
+                    codeId    
+                }
+                product_shop: [
+                    {
+                        productId,
+                        name,
+                        price,
+                        quantity,
+                        isSelected
+                    }
+                ]
+            }
+        ]    
+    */
+    cart_count_product: {type: Number, default: 1},
+    userId: {type: Schema.Types.ObjectId, required: true, ref: 'User'}
 }, {
     collection: COLLECTION_NAME,
     timestamps: {
@@ -23,6 +50,4 @@ const cartSchema = new Schema({
     }
 })
 
-module.exports = {
-    cart: model(DOCUMENT_NAME, cartSchema)
-}
+module.exports = model(DOCUMENT_NAME, cartSchema);
