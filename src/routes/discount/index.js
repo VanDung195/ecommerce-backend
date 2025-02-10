1c560e73142aabfbac484662b40f1d7ffc1c7715
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-10 22:32:32 +0700

    feat: add routers for discount

--- src/routes/discount/index.js
+++ src/routes/discount/index.js
@@ -5,8 +5,11 @@ const { authentication } = require('../../auth/authUtils')
 const router = express.Router()
 const { asyncHandler } = require('../../helpers/asyncHandler')
 const discountController = require('../../controllers/discount.controller')
+const { checkShopPermission } = require('../../middleware/shop.middleware')
 
 router.use(authentication)
-router.post('/create', asyncHandler(discountController.newDiscount))
+router.post('/create', checkShopPermission, asyncHandler(discountController.newDiscount))
+router.get('/:code', checkShopPermission, asyncHandler(discountController.getOneDiscount))
+router.post('/get_recommend_discount', asyncHandler(discountController.getRecommendDiscount))
 
 module.exports = router
