b0103421e1db20408d079ab1f0253bfb940f182f
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-10 22:07:57 +0700

    change discount_productIds field

--- src/models/discount.model.js
+++ src/models/discount.model.js
@@ -21,14 +21,19 @@ var discountSchema = new Schema({
     discount_start_date: { type: Date, required: true},
     discount_end_date: { type: Date, required: true},
     discount_max_uses: { type: Number, required: true},
-    discount_uses_count: { type: Number, default: 0},
+    discount_uses_count: { type: Number, default: 0}, //auto increment
     discount_user_used: { type: Array, default: []},
     discount_max_use_per_user: { type: Number, required: true},
     discount_min_order_value: { type: Number, required: true},
     discount_shopId: { type: Schema.Types.ObjectId, ref: 'Shop', required: true, index: true},
     discount_is_active: { type: Boolean, default: true, index: true},
     discount_applies_to: { type: String, enum: ['all', 'specific']},
-    discount_productIds: { type: [Schema.Types.ObjectId], ref: 'Sku', required: true}
+    discount_productIds: { type: [String], ref: 'Sku',
+        required: function(){
+            return this.discount_applies_to === 'specific'
+        }
+    },
+    isDeleted: { type: Boolean, default: false}
 }, {
     timestamps: true,
     collection: COLLECTION_NAME
