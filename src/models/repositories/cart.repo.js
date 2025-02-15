75853d50dc6c12ec962723236e78807409f305a2
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-15 19:35:05 +0700

    feat: delete getCartForOrder function

--- src/models/repositories/cart.repo.js
+++ src/models/repositories/cart.repo.js
@@ -73,248 +73,6 @@ const getCartByUserId = async ({
     })
     return cart
 }
-
-const getCartForOrder = async({
-    userId
-}) => {
-    const cart = await CART.aggregate([
-        {
-            $match: {
-                userId: convertToObjectIdMongodb(userId)
-            }
-        },
-        {
-            $addFields: {
-                cart_products: {
-                    $filter: {
-                        input: {
-                            $map: {
-                                input: '$cart_products',
-                                as: 'cart_product',
-                                in: {
-                                    $mergeObjects: [
-                                        '$$cart_product',
-                                        {
-                                            product_shop: {
-                                                $filter: {
-                                                    input: '$$cart_product.product_shop',
-                                                    as: 'product',
-                                                    cond: { $eq: ['$$product.isSelected', true] }
-                                                }
-                                            }
-                                        }
-                                    ]
-                                }
-                            }
-                        },
-                        as: 'cart_product',
-                        cond: { $gt: [{ $size: '$$cart_product.product_shop' },0]}
-                    }
-                }
-            }
-        },
-        {
-            $lookup: {
-                from: 'Shops',
-                localField: 'cart_products.shopId',
-                foreignField: '_id',
-                as: 'shop_info'
-            }
-        },
-        {
-            $addFields: {
-                cart_products: {
-                    $map: {
-                        input: '$cart_products',
-                        as: 'product',
-                        in: {
-                            $mergeObjects: [
-                                '$$product',
-                                {
-                                    shop_info: {
-                                        $arrayElemAt: [
-                                            {
-                                                $filter: {
-                                                    input: '$shop_info',
-                                                    as: 'shop',
-                                                    cond: {
-                                                        $eq: ['$$shop._id', '$$product.shopId']
-                                                    }
-                                                }
-                                            },
-                                            0
-                                        ]
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                }
-            }
-        },
-        {
-            $unset: 'shop_info'
-        },
-        {
-            $lookup: {
-                from: 'Skus',
-                localField: 'cart_products.product_shop.productId',
-                foreignField: 'skuId',
-                as: 'sku_info'
-            }
-        },
-        {
-            $addFields: {
-                cart_products: {
-                    $map: {
-                        input: '$cart_products',
-                        as: 'cart_product',
-                        in: {
-                            $mergeObjects: [
-                                '$$cart_product',
-                                {
-                                    product_shop: {
-                                        $map: {
-                                            input: '$$cart_product.product_shop',
-                                            as: 'product',
-                                            in: {
-                                                $mergeObjects: [
-                                                    '$$product',
-                                                    {
-                                                        sku_info: {
-                                                            $arrayElemAt: [
-                                                                {
-                                                                    $filter: {
-                                                                        input: '$sku_info',
-                                                                        as: 'sku',
-                                                                        cond: {
-                                                                            $eq: ['$$sku.skuId', '$$product.productId']
-                                                                        }
-                                                                    }
-                                                                },
-                                                                0
-                                                            ]
-                                                        }
-                                                    }
-                                                ]
-                                            }
-                                        }
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                }
-            }
-        },
-        {
-            $unset: 'sku_info'
-        },
-        {
-            $project: {
-                cart_state: 0,
-                _id: 0,
-                'cart_products.product_shop.sku_info._id': 0,
-                'cart_products.product_shop.sku_info.skuId': 0,
-                'cart_products.product_shop.sku_info.sku_default': 0,
-                'cart_products.product_shop.sku_info.sku_slug': 0,
-                'cart_products.product_shop.sku_info.isDraft': 0,
-                'cart_products.product_shop.sku_info.isPublished': 0,
-                'cart_products.product_shop.sku_info.isDeleted': 0,
-                'cart_products.product_shop.sku_info.createdAt': 0,
-                'cart_products.product_shop.sku_info.updatedAt': 0,
-                'cart_products.product_shop.sku_info.__v': 0,
-                'cart_products.shop_info._id': 0,
-                'cart_products.shop_info.userId': 0,
-                'cart_products.shop_info.shop_phone': 0,
-                'cart_products.shop_info.shop_status': 0,
-                'cart_products.shop_info.shop_verify': 0,
-                'cart_products.shop_info.shop_description': 0,
-                'cart_products.shop_info.shop_type': 0,
-                'cart_products.shop_info.createdAt': 0,
-                'cart_products.shop_info.updatedAt': 0,
-                'cart_products.shop_info.__v': 0,
-            }
-        },
-        {
-            $lookup: {
-                from: 'Spus',
-                localField: 'cart_products.product_shop.sku_info.productId',
-                foreignField: '_id',
-                as: 'spu_info'
-            }
-        },
-        {
-            $addFields: {
-                cart_products: {
-                    $map: {
-                        input: '$cart_products',
-                        as: 'cart_product',
-                        in: {
-                            $mergeObjects: [
-                                '$$cart_product',
-                                {
-                                    product_shop: {
-                                        $map: {
-                                            input: '$$cart_product.product_shop',
-                                            as: 'product',
-                                            in: {
-                                                $mergeObjects: [
-                                                    '$$product',
-                                                    {
-                                                        product_info: {
-                                                            $arrayElemAt: [
-                                                                {
-                                                                    $filter: {
-                                                                        input: '$spu_info',
-                                                                        as: 'spu',
-                                                                        cond: { 
-                                                                            $eq: ['$$spu._id', '$$product.sku_info.productId']
-                                                                        }
-                                                                    }
-                                                                }, 
-                                                                0
-                                                            ]
-                                                        }
-                                                    }
-                                                ]
-                                            }
-                                        }
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                }
-            }
-        },
-        {
-            $unset: 'spu_info'
-        },
-        {
-            $project: {
-                'cart_products.product_shop.product_info._id': 0,
-                'cart_products.product_shop.product_info.product_price': 0,
-                'cart_products.product_shop.product_info.product_min_price': 0,
-                'cart_products.product_shop.product_info.product_max_price': 0,
-                'cart_products.product_shop.product_info.product_category': 0,
-                'cart_products.product_shop.product_info.product_quantity': 0,
-                'cart_products.product_shop.product_info.product_shop': 0,
-                'cart_products.product_shop.product_info.isDraft': 0,
-                'cart_products.product_shop.product_info.isPublished': 0,
-                'cart_products.product_shop.product_info.isDeleted': 0,
-                'cart_products.product_shop.product_info.createdAt': 0,
-                'cart_products.product_shop.product_info.updatedAt': 0,
-                'cart_products.product_shop.product_info.__v': 0,
-                'cart_products.product_shop.product_info.product_variations.images': 0,
-                cart_count_product: 0,
-                __v: 0
-            }
-        }
-    ])
-    return cart[0]
-}
-
 const updateCartCount = async ({
     userId,
     quantity
@@ -648,7 +406,7 @@ const getListProductFromCart = async({
     ])
     return cart[0]
 }
-
+/*
 const selectProductFromCart = async ({ userId, shopId, productId }) => {
     shopId = convertToObjectIdMongodb(shopId)
     const cart = await CART.findOneAndUpdate(
@@ -704,7 +462,7 @@ const selectProductFromCart = async ({ userId, shopId, productId }) => {
     )
     return cart
 }
-
+*/
  /*
         [
             {
@@ -775,6 +533,16 @@ const removeDiscountProductCart = async({
     return cart
 }
 
+const checkProductFromCart = async({
+    userId,
+    shopId,
+    productId
+}) => {
+    // const products = CART.aggregate([
+
+    // ])
+}
+
 module.exports = {
     createCart,
     getCartByUserId,
@@ -784,9 +552,8 @@ module.exports = {
     clearCart,
     updateCartCount,
     getListProductFromCart,
-    selectProductFromCart,
     removeCartShop,
     applyDiscountProductCart,
     removeDiscountProductCart,
-    getCartForOrder
+    checkProductFromCart
 }
