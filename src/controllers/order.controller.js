e14b98ca24a18351e878f6af574f9e0d5a3c2c66
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-20 20:30:35 +0700

    feat: add orderByUser controller

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,13 +1,23 @@
 'use strict'
 
 const { SuccessResponse } = require("../core/success.response")
-const { checkoutOrderReviewService } = require("../services/order.service")
+const { checkoutOrderReviewService, createOrderService } = require("../services/order.service")
 
 class OrderController{
     checkout = async(req, res, next) => {
         new SuccessResponse({
-            message: 'Checkout order success',
+            message: 'Checkout review order success',
             metadata: await checkoutOrderReviewService({ 
+                userId: req.user.userId,
+                ...req.body
+            })
+        }).send(res)
+    }
+
+    orderByUser = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Order success',
+            metadata: await createOrderService({
                 userId: req.user.userId,
                 ...req.body
             })
