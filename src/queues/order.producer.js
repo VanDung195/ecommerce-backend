a6d1d0a100da0e76123a87261d724997ba907617
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-02-27 22:40:16 +0700

    feat: add order producer

---
+++ src/queues/order.producer.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const { connectToRabbitMQ } = require('../dbs/init.rabbit')
+
+const producerOrderMessage = async ({userId, orderProducts}) => {
+    try {
+        const { connection, channel } = await connectToRabbitMQ()
+
+        const orderQueue = 'orderQueueProcess'
+        const orderExchange = 'orderEx'
+        const orderExchangeDLX = 'orderExDLX'
+        const orderRoutingKeyDLX = 'orderRoutingKeyDLX'
+
+        //1. create Exchange
+        await channel.assertExchange(orderExchange, 'direct', {
+            durable: true
+        })
+
+        //2. create queue
+        const queueResult = await channel.assertQueue(orderQueue, {
+            exclusive: false,
+            deadLetterExchange: orderExchangeDLX,
+            deadLetterRoutingKey: orderRoutingKeyDLX
+        })
+
+        //3. binding key
+        await channel.bindQueue(queueResult.queue, orderExchange)
+
+        const payload = {
+            userId,
+            orderProducts
+        }
+        await channel.sendToQueue(queueResult.queue, Buffer.from(JSON.stringify(payload)), {
+            expiration: 10000,
+            persistent: true
+        })
+        setTimeout(() => {
+            connection.close()
+        }, 500);
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+module.exports = {
+    producerOrderMessage
+}
