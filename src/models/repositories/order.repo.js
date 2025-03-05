348486708db2a8c542feddce7ffb0d779d6ffbdf
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-05 23:19:13 +0700

    feat: add getOneOrderByUser and update pipeline for getAllOrder

--- src/models/repositories/order.repo.js
+++ src/models/repositories/order.repo.js
@@ -152,50 +152,174 @@ const getAllOrder = async({ userId, limit, page }) => {
                 as: 'sku_info'
             }
         },
-        // {
-        //     $addFields: {
-        //         'order_products.item_products': {
-        //             $map: {
-        //                 input: '$order_products.item_products',
-        //                 as: 'product',
-        //                 in: {
-        //                     $mergeObjects: [
-        //                         '$$product',
-        //                         {
-        //                             sku_info: {
-        //                                 $arrayElemAt: [
-        //                                     {
-        //                                         $filter: {
-        //                                             input: '$sku_info',
-        //                                             as: 'sku',
-        //                                             cond: {
-        //                                                 $eq: ['$$product.productId', '$$sku.skuId']
-        //                                             }
-        //                                         }
-        //                                     },
-        //                                     0
-        //                                 ]
-        //                             }    
-        //                         }
-        //                     ]
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
+        {
+            $addFields: {
+                order_products: {
+                    $map: {
+                        input: '$order_products',
+                        as: 'order_product',
+                        in: {
+                            $mergeObjects: [
+                                '$$order_product',
+                                {
+                                    item_products: {
+                                        $cond: {
+                                            if: {
+                                                $isArray: '$$order_product.item_products',
+                                            },
+                                            then: {
+                                                $map: {
+                                                    input: '$$order_product.item_products',
+                                                    as: 'item_product',
+                                                    in: {
+                                                        $mergeObjects: [
+                                                            '$$item_product',
+                                                            {
+                                                                $arrayElemAt: [
+                                                                    {
+                                                                        $filter: {
+                                                                            input: '$sku_info',
+                                                                            as: 'sku',
+                                                                            cond: { $eq: ['$$item_product.productId', '$$sku.skuId']}
+                                                                        }
+                                                                    },
+                                                                    0
+                                                                ]
+                                                            }
+                                                        ]
+                                                    }
+                                                }
+                                            },
+                                            else: []
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        },
+        {
+            $unset: 'sku_info'
+        },
+        {
+            $project: {
+                'order_products.item_products._id': 0,
+                'order_products.item_products.sku_default': 0,
+                'order_products.item_products.sku_slug': 0,
+                'order_products.item_products.sku_price': 0,
+                'order_products.item_products.isDraft': 0,
+                'order_products.item_products.isPublished': 0,
+                'order_products.item_products.isDeleted': 0,
+                'order_products.item_products.createdAt': 0,
+                'order_products.item_products.updatedAt': 0,
+                'order_products.item_products.__v': 0,
+            }
+        },
+        {
+            $lookup: {
+                from: 'Spus',
+                localField: 'order_products.item_products.productId',
+                foreignField: '_id',
+                as: 'spu_info'
+            }
+        },
+        {
+            $addFields: {
+                order_products: {
+                    $map: {
+                        input: '$order_products',
+                        as: 'order_product',
+                        in: {
+                            $mergeObjects: [
+                                '$$order_product',
+                                {
+                                    item_products: {
+                                        $cond: {
+                                            if: {
+                                                $isArray: '$$order_product.item_products',
+                                            },
+                                            then: {
+                                                $map: {
+                                                    input: '$$order_product.item_products',
+                                                    as: 'item_product',
+                                                    in: {
+                                                        $mergeObjects: [
+                                                            '$$item_product',
+                                                            {
+                                                                $arrayElemAt: [
+                                                                    {
+                                                                        $filter: {
+                                                                            input: '$spu_info',
+                                                                            as: 'spu',
+                                                                            cond: { $eq: ['$$item_product.productId', '$$spu._id']}
+                                                                        }
+                                                                    },
+                                                                    0
+                                                                ]
+                                                            }
+                                                        ]
+                                                    }
+                                                }
+                                            },
+                                            else: []
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        },
+        {
+            $unset: 'spu_info'
+        },
+        {
+            $project: {
+                'order_products.item_products.product_shop': 0,
+                'order_products.item_products.product_price': 0,
+                'order_products.item_products.product_description': 0,
+                'order_products.item_products._id': 0,
+                'order_products.item_products.product_min_price': 0,
+                'order_products.item_products.product_max_price': 0,
+                'order_products.item_products.product_category': 0,
+                'order_products.item_products.product_quantity': 0,
+                'order_products.item_products.isDraft': 0,
+                'order_products.item_products.isPublished': 0,
+                'order_products.item_products.isDeleted': 0,
+                'order_products.item_products.createdAt': 0,
+                'order_products.item_products.updatedAt': 0,
+                'order_products.item_products.__v': 0,
+            }
+        },
+        { $skip: skip},
+        { $limit: limit}
     ]);
-    return orders
+    const totalOrders = await ORDER.countDocuments(filter)
+    return {
+        data: orders,
+        limit,
+        page,
+        totalPages: Math.ceil(totalOrders / limit)
+    }
 }
-// const totalOrders = await ORDER.countDocuments(filter)
-// return {
-//     data: orders,
-//     limit,
-//     page,
-//     totalPages: Math.ceil(totalOrders / limit)
-// }
+
+const getOneOrderByUser = async({ userId, orderId }) => {
+    return await ORDER.findOne({
+        order_userId: userId,
+        _id: convertToObjectIdMongodb(orderId)
+    })
+}
+
+const cancelOrder = async({ userId, orderId }) => {
+
+}
 
 module.exports = {
     createOrder,
     getOrderByUser,
-    getAllOrder
+    getAllOrder,
+    getOneOrderByUser
 }
