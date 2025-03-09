b1c5fe6df0dc25fd5e2ca38a2bdc97f45a0d677f
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-09 22:54:13 +0700

    feat: add routes

--- src/routes/order/index.js
+++ src/routes/order/index.js
@@ -11,4 +11,9 @@ router.post('/checkout', asyncHandler(orderController.checkout))
 router.post('/', asyncHandler(orderController.orderByUser))
 router.get('/get_all_orders', asyncHandler(orderController.getAllOrderByUser))
 router.post('/cancel', asyncHandler(orderController.cancelOrderByUser))
+router.post('/confirm', asyncHandler(orderController.confirmOrderByShop))
+router.post('/shipping', asyncHandler(orderController.shippingOrderByShop))
+router.post('/delivery', asyncHandler(orderController.deliveryOrderByShop))
+router.post('/complete', asyncHandler(orderController.completeOrderByShop))
+
 module.exports = router
