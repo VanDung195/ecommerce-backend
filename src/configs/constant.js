7b8d61ce129f8475ebf8a0c4f11fcfda6f943c0c
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-10 23:19:04 +0700

    feat: add ORDER_STATUSES constant variable

--- src/configs/constant.js
+++ src/configs/constant.js
@@ -1,5 +1,15 @@
 module.exports = {
     CACHE_KEYSTORE: {
         'REFRESH_TOKEN': 'rt-'
+    },
+    ORDER_STATUSES: {
+        9: 'pending',
+        2: 'confirmed',
+        7: 'shipping',
+        8: 'pending_delivery',
+        3: 'completed',
+        4: 'cancelled',
+        12: 'returned',
+        6: 'all'
     }
 }
