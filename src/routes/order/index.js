1a7cbef846b2e1a93b9284cbf6971071aabc0385
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-10 23:10:36 +0700

    refactor: add checkShopPermission middleware

--- src/routes/order/index.js
+++ src/routes/order/index.js
@@ -5,15 +5,19 @@ const router = express.Router()
 const { authentication } = require('../../auth/authUtils')
 const { asyncHandler } = require('../../helpers/asyncHandler')
 const orderController = require('../../controllers/order.controller')
+const { checkShopPermission } = require('../../middleware/shop.middleware')
 
 router.use(authentication)
 router.post('/checkout', asyncHandler(orderController.checkout))
 router.post('/', asyncHandler(orderController.orderByUser))
 router.get('/get_all_orders', asyncHandler(orderController.getAllOrderByUser))
 router.post('/cancel', asyncHandler(orderController.cancelOrderByUser))
-router.post('/confirm', asyncHandler(orderController.confirmOrderByShop))
-router.post('/shipping', asyncHandler(orderController.shippingOrderByShop))
-router.post('/delivery', asyncHandler(orderController.deliveryOrderByShop))
-router.post('/complete', asyncHandler(orderController.completeOrderByShop))
+router.post('/confirm', checkShopPermission, asyncHandler(orderController.confirmOrderByShop))
+router.post('/shipping', checkShopPermission, asyncHandler(orderController.shippingOrderByShop))
+router.post('/delivery', checkShopPermission, asyncHandler(orderController.deliveryOrderByShop))
+router.post('/complete', checkShopPermission, asyncHandler(orderController.completeOrderByShop))
+
+router.post('/confirm_cancelled_order', checkShopPermission, asyncHandler(orderController.confirmCancelledOrderByShop))
+router.get('/get_all_orders_shop', checkShopPermission, asyncHandler(orderController.getAllOrderByShop))
 
 module.exports = router
