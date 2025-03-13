a5c2dd4da6df10ff26174e086ad6fcc054ed60d8
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-13 22:20:47 +0700

    refactor: update

--- src/middleware/cache.middleware.js
+++ src/middleware/cache.middleware.js
@@ -1,17 +1,20 @@
 'use strict'
 
 const { CACHE_PRODUCT } = require("../configs/constant")
+const { SuccessResponse } = require("../core/success.response")
 const { getCache } = require("../models/repositories/cache.repo")
 
 const readProductCache = async(req, res, next) => {
     const { slug } = req.params
     const productDetailCache = `${CACHE_PRODUCT.PRODUCT_DETAIL}${slug}`
-    const productDetail = getCache({ key: productDetailCache })
+    const productDetail = await getCache({ key: productDetailCache })
+    if(!productDetail) 
+        return next()
     if(productDetail){
-        return res.status(200).json({
-            ...JSON.parse(productDetail),
-            toLoad: 'cache middleware'
-        })
+        return new SuccessResponse({
+            message: 'Success Cache',
+            metadata: JSON.parse(productDetail)
+        }).send(res)
     }
 }
 
