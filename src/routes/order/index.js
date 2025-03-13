729e96572164ab0f1ce2191747e7dd7a8c0655a9
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-13 22:22:38 +0700

    feat: add routers for orders

--- src/routes/order/index.js
+++ src/routes/order/index.js
@@ -12,12 +12,16 @@ router.post('/checkout', asyncHandler(orderController.checkout))
 router.post('/', asyncHandler(orderController.orderByUser))
 router.get('/get_all_orders', asyncHandler(orderController.getAllOrderByUser))
 router.post('/cancel', asyncHandler(orderController.cancelOrderByUser))
+router.get('/detail/:orderId', asyncHandler(orderController.getOrderDetailByUser))
+//shop
 router.post('/confirm', checkShopPermission, asyncHandler(orderController.confirmOrderByShop))
 router.post('/shipping', checkShopPermission, asyncHandler(orderController.shippingOrderByShop))
 router.post('/delivery', checkShopPermission, asyncHandler(orderController.deliveryOrderByShop))
 router.post('/complete', checkShopPermission, asyncHandler(orderController.completeOrderByShop))
+router.get('/detail/shop/:orderId', checkShopPermission, asyncHandler(orderController.getOrderDetailByShop))
 
-router.post('/confirm_cancelled_order', checkShopPermission, asyncHandler(orderController.confirmCancelledOrderByShop))
+router.post('/confirm_cancellation_order', checkShopPermission, asyncHandler(orderController.confirmOrderCancellationByShop))
+router.post('/reject_cancellation_order', checkShopPermission, asyncHandler(orderController.rejectOrderCancellationByShop))
 router.get('/get_all_orders_shop', checkShopPermission, asyncHandler(orderController.getAllOrderByShop))
 
 module.exports = router
