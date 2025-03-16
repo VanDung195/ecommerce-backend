58d989ec03c782eaf313bb192d2670adcf4dea34
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-16 23:12:42 +0700

    feat: add consumer for order cancellation

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({
 //init db
 require('./dbs/init.mongodb')
 const ioredis = require('./dbs/init.ioredis')
-const { consumerOrderFailed, consumerOrderNormal } = require('./queues/order.consumer')
+const { consumerOrderFailed, consumerOrderNormal, consumerOrderCancellation } = require('./queues/order.consumer')
 ioredis.init({
     IOREDIS_HOST: 'localhost',
     IOREDIS_PORT: 6379,
@@ -28,6 +28,7 @@ app.use('/', require('./routes'))
 
 // consumerOrderNormal().catch(console.error)
 // consumerOrderFailed().catch(console.error)
+consumerOrderCancellation().catch(console.error)
 
 //error handler
 app.use((req, res, next) => {
