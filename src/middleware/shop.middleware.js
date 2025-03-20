0961a6b6eb9a2212e65cc08eb2fd0bb568fc0f8e
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-20 22:34:08 +0700

    feat: add check shop valid

--- src/middleware/shop.middleware.js
+++ src/middleware/shop.middleware.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { AuthFailureError } = require("../core/error.response")
+const { AuthFailureError, BadRequestError } = require("../core/error.response")
 const { findShopByUserId } = require("../models/repositories/shop.repo")
 
 const checkShopPermission = async (req, res, next) => {
@@ -8,7 +8,8 @@ const checkShopPermission = async (req, res, next) => {
         const user = req.user
         const foundShop = await findShopByUserId({ userId: user.userId })
         if (!foundShop) throw new AuthFailureError('Shop not registered')
-
+        if(!foundShop.shop_verify) throw new BadRequestError('Unverified shop')
+        if(foundShop.shop_status !== 'active') throw new BadRequestError('Shop has been banned')
         req.shop = foundShop
         next()
     } catch (error) {
