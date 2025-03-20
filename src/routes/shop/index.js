f365e7872705007302b2c05aa4831dbae9f8ecfc
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-20 22:36:46 +0700

    feat: update router for shop

--- src/routes/shop/index.js
+++ src/routes/shop/index.js
@@ -5,11 +5,14 @@ const {asyncHandler} = require('../../helpers/asyncHandler')
 const { authentication } = require('../../auth/authUtils')
 const { newShop } = require('../../controllers/shop.controller')
 const shopController = require('../../controllers/shop.controller')
+const { checkShopPermission } = require('../../middleware/shop.middleware')
 const router = express.Router()
 
 router.use(authentication)
 router.post('/new_shop', asyncHandler(shopController.newShop))
-router.get('/my_shop', asyncHandler(shopController.getShopByUserId))
-router.get('/', asyncHandler(shopController.getShopByUserId))
+router.get('/my_shop', checkShopPermission, asyncHandler(shopController.getShopByUserId))
+router.delete('/', checkShopPermission, asyncHandler(shopController.deleteShopByUser))
+//admin
+router.get('/', asyncHandler(shopController.getAllShop))
 
 module.exports = router
