96e190c16ce87ffae0f2ed40096f16ef497d01f6
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-27 20:11:31 +0700

    feat: add new controllers for seller affiliate and partner affiliate

--- src/controllers/affiliate.controller.js
+++ src/controllers/affiliate.controller.js
@@ -2,17 +2,44 @@
 
 const { SuccessResponse } = require("../core/success.response")
 const { createAffiliateService, verifyAffiliateService, rejectAffiliateService, recordClickService } = require("../services/affiliate.service")
+const { createPartnerAffiliateService } = require("../services/partnerAffiliate.service")
+const { createSellerAfiiliateService, createAffiliateLinkBySellerService } = require("../services/sellerAffiliate.service")
 
 class AffiliateController{
-    newAffiliate = async(req, res, next) => {
+    //seller affiliate
+    newSellerAffiliate = async(req, res, next) => {
         new SuccessResponse({
-            message: 'Success',
-            metadata: await createAffiliateService({
+            message: 'Created seller affiliate',
+            metadata: await createSellerAfiiliateService({
                 userId: req.user.userId,
+                shopId: req.shop._id,
                 ...req.body
             })
         }).send(res)
     }
+    createAffiliateLinkBySeller = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Create affiliate link success',
+            metadata: await createAffiliateLinkBySellerService({
+                userId: req.user.userId,
+                affiliateId: req.affiliate._id,
+                productId: req.body.productId
+            })
+        }).send(res)
+    }
+    //end seller affiliate
+
+    //partner affiliate (KOL, KOC, OTHER)
+    newPartnerAffiliate = async(req, res, next) => {
+        new SuccessResponse({
+            message: 'Created partner affiliate',
+            metadata: await createPartnerAffiliateService({
+                userId: req.user.userId,
+                ...req.body
+            })
+        }).send(res)
+    } 
+    //end partner affiliate
 
     verifyAffiliate = async(req, res, next) => {
         new SuccessResponse({
