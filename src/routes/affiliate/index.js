02c8d98bfb32da7465b420a511a4d1cf187722b9
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-27 20:13:05 +0700

    feat: add more routers

--- src/routes/affiliate/index.js
+++ src/routes/affiliate/index.js
@@ -6,10 +6,18 @@ const { asyncHandler } = require('../../helpers/asyncHandler')
 const { authentication } = require('../../auth/authUtils')
 const affiliateController = require('../../controllers/affiliate.controller')
 const { grantAccess } = require('../../middleware/rbac')
+const { checkShopPermission } = require('../../middleware/shop.middleware')
+const checkSellerAffiliatePermission = require('../../middleware/sellerAffiliate.middleware')
 
 router.get('/record_click', asyncHandler(affiliateController.recordClick))
 
 router.use(authentication)
+//seller
+router.post('/seller/create', checkShopPermission, asyncHandler(affiliateController.newSellerAffiliate))
+router.post('/seller/affiliate-links', checkSellerAffiliatePermission, asyncHandler(affiliateController.createAffiliateLinkBySeller))
+//partner
+router.post('/partner/create', asyncHandler(affiliateController.newPartnerAffiliate))
+
 router.post('', asyncHandler(affiliateController.newAffiliate))
 
 
