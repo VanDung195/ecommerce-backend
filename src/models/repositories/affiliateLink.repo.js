c1f6f6e37f84c7617629dcd6550d72bb75f0663b
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-29 20:09:05 +0700

    feat: add functions for affiliate link

--- src/models/repositories/affiliateLink.repo.js
+++ src/models/repositories/affiliateLink.repo.js
@@ -11,7 +11,28 @@ const createAffiliateLink = async({ affiliateId, productId, destination_url, sho
     })
 }
 
+const getOneAffiliateLinkByShortUrl = async(short_url) => {
+    return AFFILIATE_LINK.findOne({ short_url })
+}
+
+const getOneAffiliateLinkByAffIdAndProductId = async({ affiliateId, productId }) => {
+    return AFFILIATE_LINK.findOne({
+        affiliateId,
+        productId
+    })
+}
+
+const incrementClickCount = async(affiliateId) => {
+    return AFFILIATE_LINK.findOneAndUpdate(
+        { _id: affiliateId },
+        { $inc: { click_count: 1 } },
+        { new: true }
+    )
+}
 
 module.exports = {
-    createAffiliateLink
+    createAffiliateLink,
+    getOneAffiliateLinkByShortUrl,
+    getOneAffiliateLinkByAffIdAndProductId,
+    incrementClickCount
 }
