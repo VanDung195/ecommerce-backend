4dcdb4702b97818d35d2128e32c162c6479190c7
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-29 20:08:27 +0700

    update model

--- src/models/affiliateConversion.model.js
+++ src/models/affiliateConversion.model.js
@@ -7,13 +7,13 @@ const COLLECTION_NAME = 'AffiliateConversions'
 
 var affiliateConversionSchema = new Schema({
     affiliate_link: { type: Types.ObjectId, ref: 'AffiliateLinks', required: true },
-    click: { type: Types.ObjectId, ref: 'AffiliateClicks', required: true },
-    order: { type: Types.ObjectId, ref: 'Orders', required: true },
-    shop: { type: Types.ObjectId, ref: 'Shops', required: true },
-    conversion_date: { type: Types.ObjectId, default: () => new Date() },
+    orderId: { type: Types.ObjectId, ref: 'Orders', required: true },
+    shopId: { type: Types.ObjectId, ref: 'Shops', required: true },
+    conversion_date: { type: Date, default: () => new Date() },
     order_value: { type: Number },
     commission_amount: { type: Number, required: true },
-    isSelfAffiliate: { type: Boolean, default: false },
+    // isSelfAffiliate: { type: Boolean, default: false },
+    status: { type: String, enum: ['pending', 'complete', 'cancelled'], default: 'pending'}
 }, {
     timestamps: true,
     collection: COLLECTION_NAME
