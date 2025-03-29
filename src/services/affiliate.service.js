c58cd0a975153433925765bb0f7ede1fdea8f176
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-29 20:12:56 +0700

    feat: add services for redirecting destination url and handling affiliate url click

--- src/services/affiliate.service.js
+++ src/services/affiliate.service.js
@@ -3,24 +3,89 @@
 const { v4: uuidv4 } = require('uuid')
 const { BadRequestError, NotFoundError } = require("../core/error.response")
 const { createAffiliate, getOneAffiliateById, verifyAffiliate, rejectAffiliate } = require("../models/repositories/affiliate.repo")
+const { getCache, setCacheExpiration } = require('../models/repositories/cache.repo')
+const { addRole } = require('../models/repositories/user.repo')
+const { getOneAffiliateLinkByShortUrl, getOneAffiliateLinkByAffIdAndProductId, incrementClickCount } = require('../models/repositories/affiliateLink.repo')
+const { getOneSellerAffiliateById } = require('../models/repositories/sellerAffiliate.repo')
+const { getOneSpuBySlug, getOneSpuDetailForCustomer } = require('../models/repositories/spu.repo')
+const { AFFILIATE } = require('../configs/constant')
+const { recordClick } = require('../models/repositories/affiliateClick.repo')
 
+const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const encodeBase62 = (num) => {
+    let result = '';
+    while (num > 0) {
+        result = characters[num % 62] + result;
+        num = Math.floor(num / 62);
+    }
+    return result || '0';
+}
 
-//user
-const createAffiliateService = async({ userId, social_media = [] }) => {
-    const affiliate = await createAffiliate({ userId, social_media})
+const createAffiliateService = async({ userId, social_media = [], type }) => {
+    const affiliate = await createAffiliate({ userId, social_media, type })
     if(!affiliate)
         throw new BadRequestError('Create affiliate failed')
+    await addRole({ userId, role: 'affiliate'})
     return affiliate
 }
-//end user
 
-const recordClickService = ({ affiliateLinkId = '1', ipAddress, userAgent = '' }) => {
-    if(ipAddress === '::1'){ //IPv6 to IPv4
-        ipAddress = '172.0.0.1'
-    }
-    console.log(userAgent)
-    return uuidv4()
+const redirectToDestinationUrlService = async(short_url) => {
+    const affiliateLink = await getOneAffiliateLinkByShortUrl(short_url)
+    if(!affiliateLink)
+        throw new NotFoundError('Affiliate link not found')
+    //set cookie để tiếp thị gián tiếp?
+    return affiliateLink.destination_url
 }
+
+const handlerDestinationUrlService = async({ product_slug, type ,affiliateId, ip_address, user_agent, country, device_type, browser }) => {
+    if(type !== 'affiliates')
+        throw new BadRequestError('Bad request')
+    const [affPrefix, affSuffix] = affiliateId.split('_'); //affiliate type and affiliateId
+    if (!affPrefix || !affSuffix) 
+        throw new BadRequestError('Invalid request')
+    let foundAffiliate = null
+    if(affPrefix === 'sl'){ //sl is seller affiliate
+        foundAffiliate = await getOneSellerAffiliateById(affSuffix)
+    } else if(affPrefix === 'pa') { //pa is partner affiliate (KOL, KOC, ...)
+        //getOnePartnerAffiliate
+    } else {
+        throw new BadRequestError('Unknow affiliate type')
+    }
+    if(!foundAffiliate)
+        throw new NotFoundError('Affiliate not found')
+    const foundProduct = await getOneSpuBySlug(product_slug)
+    if(!foundProduct)
+        throw new NotFoundError('Product not found')
+    const foundAffiliateLink = await getOneAffiliateLinkByAffIdAndProductId({ affiliateId: foundAffiliate._id, productId: foundProduct._id })
+    if(!foundAffiliateLink)
+        throw new NotFoundError('Invalid affiliate link')
+    const productDetail = await getOneSpuDetailForCustomer(foundProduct._id)
+
+    //record click and update click_count in affiliate link
+    await incrementClickCount(foundAffiliate._id)
+    if(ip_address === '::1')
+        ip_address = '172.0.0.1'
+    const affClickKey = `${AFFILIATE.AFFILIATE_CLICK}:${ip_address}_${user_agent}_${foundProduct._id.toString()}`
+    const affiliateClick = await getCache({ key: affClickKey })
+    if(!affiliateClick){
+        console.log('Affiliate click not exists in cache')
+        await setCacheExpiration({
+            key: affClickKey,
+            value: 'OK',
+            expiration: 10
+        })
+        await recordClick({ 
+            affiliate_link: foundAffiliateLink._id, 
+            ip_address,
+            user_agent,
+            country,
+            device_type,
+            browser
+        })
+    }
+    return productDetail[0]
+}
+
 
 //admin
 const getAllAffiliates = async({
@@ -56,5 +121,6 @@ module.exports = {
     createAffiliateService,
     verifyAffiliateService,
     rejectAffiliateService,
-    recordClickService
+    redirectToDestinationUrlService,
+    handlerDestinationUrlService
 }
