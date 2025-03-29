38d13ac565f2943492b71b518c8d832c4a04197d
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-03-29 20:10:08 +0700

    feat: add fields

--- src/models/affliliateClick.model.js
+++ src/models/affliliateClick.model.js
@@ -9,6 +9,9 @@ var affiliateClickSchema = new Schema({
     affiliate_link: { type: Types.ObjectId, ref: 'AffiliateLinks', required: true, index: true },
     ip_address: {type: String, required: true },
     user_agent: { type: String, required: true },
+    country: { type: String, required: true },
+    device_type: { type: String, required: true },
+    browser: { type: String, required: true },
     clickedAt: { type: Date, default: () => new Date() }
 }, {
     timestamps: false,
