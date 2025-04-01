35faac29b0905122daa3ceb91c3d6445a46fa75d
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2025-04-01 20:32:09 +0700

    feat: add affiliate_type field to affiliate link model

--- src/models/affiliateLink.model.js
+++ src/models/affiliateLink.model.js
@@ -7,8 +7,9 @@ const COLLECTION_NAME = 'AffiliateLinks'
 
 var affiliateLinkSchema = new Schema({
     affiliateId: { type: Types.ObjectId, ref: 'Affiliates', required: true, index: true },
+    affiliate_type: { type: String, enum: ['seller', 'partner'], required: true },
     productId: { type: Types.ObjectId, ref: 'Spus', required: true },
-    destination_url: { type: String, required: true },
+    destination_url: { type: String, required: true, index: true },
     short_url: { type: String, required: true, index: true },
     click_count: { type: Number, default: 0},
     conversion_count: { type: Number, default: 0},
